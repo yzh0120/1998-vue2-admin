29a741f6d98f70ad39f34c8b158c2d0693e88e64
yzh0120 <644193385@qq.com>
2024-03-18 17:06:07 +0800

    up

--- src/components/tablePage.vue
+++ src/components/tablePage.vue
@@ -1,58 +1,64 @@
 <template>
   <div class="box_table">
-      <div class="top">
-        <slot name="form"></slot>
-      </div>
+    <div class="top" ref="top">
+      <slot name="form"></slot>
+    </div>
 
-      <div class="bottom" ref="boottom">
-        <div  :style="{height:height}" class="bbb" v-if="tableChange">
-          <slot name="table"></slot>
-        </div>
+    <div class="bottom" ref="boottom">
+      <div :style="{ height: height }" class="bbb" v-if="tableChange">
+        <slot name="table"></slot>
       </div>
+    </div>
   </div>
 </template>
 
 <script>
 import elementResizeDetectorMaker from 'element-resize-detector'
 export default {
-  data() { 
+  data() {
     return {
-      tableChange:false,
-      height:"0px"
+      tableChange: false,
+      height: "0px"
     }
   },
-  mounted() { 
+  mounted() {
     const _this = this;
     const erd = elementResizeDetectorMaker()
-    //监听
-      erd.listenTo(_this.$refs.boottom, (element) => {
-     
-      // _this.$nextTick(() => {
-        console.log(element.offsetHeight, "element.offsetHeight")
-        _this.height = element.offsetHeight + 'px'
-        this.tableChange = true
-      // })
+    //监听top
+    erd.listenTo(_this.$refs.top, (element) => {
+      console.log(element.offsetHeight, "top.offsetHeight")
+      _this.height = 0 + 'px'
+      this.tableChange = true
+    })
+    //监听Bottom
+    erd.listenTo(_this.$refs.boottom, (element) => {
+      console.log(element.offsetHeight, "element.offsetHeight")
+      _this.height = element.offsetHeight + 'px'
+      this.tableChange = true
     })
   },
-  }
+}
 </script>
 
 <style lang="scss" scoped>
-.box_table{
+.box_table {
   // background-color: green;
   height: calc(100vh - 130px);
   display: flex;
   flex-direction: column;
+
   // overflow-y: auto;
-  .top{
+  .top {
     // background-color: red;
     // height: 200px;
   }
-  .bottom{
+
+  .bottom {
     // overflow-y: auto;
     // background-color: yellow;
-    flex-grow:2;
-    .bbb{
+    flex-grow: 2;
+
+    .bbb {
       overflow-y: auto;
       // background-color: aqua;
     }
