d61059c3fd57c1cae728962a806b3fdfc6b2c0ce
yzh0120 <644193385@qq.com>
2024-04-12 15:42:43 +0800

    up

--- src/router/baseRoutes.js
+++ src/router/baseRoutes.js
@@ -46,6 +46,26 @@ const routes = [
     name: 'login',
     component: () => import("@/views/base/login.vue")
   },
+  {
+		path: '/404',
+		name: '404',
+		component: () => import( /* webpackChunkName: "404" */ "@/views/base/404.vue"),
+		meta: {
+			hidden: true,
+			title: '404',
+			// icon: 'el-icon-location',
+		}
+	},
+	{
+		path: '/401',
+		name: '401',
+		component: () => import( /* webpackChunkName: "401" */ "@/views/base/401.vue"),
+		meta: {
+			hidden: true,
+			title: '401',
+			// icon: 'el-icon-location',
+		}
+	},
   baseForm, //表单
   vxeTable,
   fastCopy,
