4a0a552c22c59c4bc987e7de3dd933946fcf1b31
yzh0120 <644193385@qq.com>
2024-07-04 18:49:04 +0800

    up

--- src/components/alert.vue
+++ src/components/alert.vue
@@ -17,10 +17,17 @@ data：{
  -->
 <template>
   <vxe-modal ref="modal" v-model="flag" :mask="false" 
-  :lockView="false" :width="data.width" :height="data.height" show-footer :title="title" @close="cancel" v-if="flag" show-zoom>
-      {{ data.width  }}
+  :lockView="false" :width="data.width" :height="data.height" show-footer :title="title" @close="cancel" v-if="flag" >
+      <!-- {{ data.width  }} -->
         <template #corner>
-          <vxe-icon name="minus" @click="narrow" style="cursor: pointer;"></vxe-icon>
+          <vxe-icon name="minus" @click="narrow" style="cursor: pointer;" v-if="active_full_narrow == `normal`"></vxe-icon>
+          <vxe-icon name="maximize" @click="narrow" style="cursor: pointer;"  v-else></vxe-icon>
+          
+          <!-- vxe-icon-   -->
+          <span style="margin-right: 10px"></span>
+          <vxe-icon name="fullscreen" @click="full" style="cursor: pointer;" v-if="active_full_narrow == `normal`"></vxe-icon>
+          <vxe-icon name="maximize" @click="full" style="cursor: pointer;"  v-else></vxe-icon>
+          
         </template>
 
           <slot></slot>
@@ -68,17 +75,35 @@ export default {
 
   },
   methods: {
-    narrow() { 
-      if (this.data.width == 200) {
+    full() { 
+      if (this.data.width == "100%") {
         this.data.width = this.oldW
         this.data.height = this.oldH
-        // this.top= "center"
-        // this.left="center"
-      } else { 
+      }else if(this.data.width == 200) { 
+        this.data.width = "100%"
+        this.data.height = "100%"
+      }
+      else { //正常的
+        this.data.width = "100%"
+        this.data.height = "100%"
+      }
+      console.log(this.data.width, "this.data.width")
+      this.$refs.modal.setPosition(0,0)
+      this.$forceUpdate()
+    },
+    narrow() { 
+      if (this.data.width == "100%") {
         this.data.width = 200
         this.data.height = 200
-        // this.top=0
-        // this.left=0
+      }
+      else if (this.data.width == 200) {
+        this.data.width = this.oldW
+        this.data.height = this.oldH
+
+      } else { //正常的
+        this.data.width = 200
+        this.data.height = 200
+
       }
       console.log(this.data.width, "this.data.width")
       this.$refs.modal.setPosition(0,0)
@@ -95,6 +120,15 @@ export default {
     },
   },
   computed: {
+    active_full_narrow() { 
+      if (this.data.width == "100%") { 
+        return "full"
+      }else if (this.data.width == 200) { 
+        return "narrow"
+      }else  { 
+        return "normal"
+      }
+    },
     flag: {
       get() {
         return this.data.alert;
