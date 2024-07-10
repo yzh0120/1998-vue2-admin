b55fc98d18c9169e79af672a7fa01484580e7078
yzh0120 <644193385@qq.com>
2024-07-10 18:34:24 +0800

    up

--- src/mixins/baseForm.js
+++ src/mixins/baseForm.js
@@ -21,9 +21,6 @@ export default {
 			}
 			return placeholder
 		},
-
-
-
 		//list变化带动data字段变化       只在组件内部使用
 		_updatedata(formInfo) { // 根据 this.formInfo.list 给data添加属性,会清空 data的值
 			formInfo.list.forEach((item) => { //循环出data的值
@@ -54,20 +51,21 @@ export default {
 			formInfo.list = temp
 		},
 		//给list每个对象添加show属性       只在组件内部使用(暂时不用)
-		_addShow(formInfo) {
-			let temp = this.$fn.deepClone(formInfo.list)
-			temp.forEach((item) => {
-				// item.disabled = false
-				if (this.$fn.type(item.show) === "und" || this.$fn.type(item.show) === "null") {
-					item.show = true
-				} else { //定义了  则不动
+		// _addShow(formInfo) {
+		// 	let temp = this.$fn.deepClone(formInfo.list)
+		// 	temp.forEach((item) => {
+		// 		// item.disabled = false
+		// 		if (this.$fn.type(item.show) === "und" || this.$fn.type(item.show) === "null") {
+		// 			item.show = true
+		// 		} else { //定义了  则不动
 
-				}
-			})
-			formInfo.list = temp
-		},
+		// 		}
+		// 	})
+		// 	formInfo.list = temp
+		// },
 
-		/* 只在组件外部使用 */
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+		只在组件外部使用 */
 		// 设置list的某个options（属性）       只在组件外部使用
 		_set(formInfo, fieldValue, valueObj) {
 			if (this.$fn.type(valueObj) == "obj") { //this._set(this.addApplyForm, "degreeType", { opt: data,xxx:xxx });
@@ -91,18 +89,5 @@ export default {
 			})
 
 		},
-		//清除值
-		// _setNull(formInfo, fieldValue, valueObj) {
-		// 	this._set(formInfo, fieldValue, Object.assign({
-		// 		//show: true,
-		// 	}), valueObj);
-		// 	this.$set(formInfo.data, fieldValue, null)
-		// },
-		//重置表单数据
-		// _reset(formInfo) {
-		// 	for (let key of Object.keys(formInfo.data)) {
-		// 		this.$set(formInfo.data, key, null)
-		// 	}
-		// }
 	}
 }
