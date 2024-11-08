77379b511b459f0bab4cc4a235f0e740301dcb36
yzh0120 <644193385@qq.com>
2024-11-08 17:00:37 +0800

    up

--- src/views/1-base-form/9-dynamicValidateForm-form.vue
+++ src/views/1-base-form/9-dynamicValidateForm-form.vue
@@ -22,7 +22,7 @@
         <el-input v-model="dynamicValidateForm.email"></el-input>
       </el-form-item>
       <!-- 域名 -->
-      <el-form-item
+      <!-- <el-form-item
         v-for="(domain, index) in dynamicValidateForm.domains"
         :label="'域名' + index"
         :key="domain.key"
@@ -35,7 +35,28 @@
       >
         <el-input v-model="domain.value"></el-input
         ><el-button @click.prevent="removeDomain(domain)">删除</el-button>
-      </el-form-item>
+      </el-form-item> -->
+
+      <el-row :gutter="20" v-for="(domain, index) in dynamicValidateForm.domains" :key="index">
+                  <!-- 域名 -->
+        <el-col :span="8">
+          <el-form-item :label="'域名1' + index" :prop="`domains.${index}.value1`" :rules="{required: true,message: '域名不能为空',trigger: 'blur',}">
+            <el-input v-model="domain.value1"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item :label="'域名2' + index" :prop="`domains.${index}.value2`" :rules="{required: true,message: '域名不能为空',trigger: 'blur',}">
+            <el-input v-model="domain.value2"></el-input>
+          </el-form-item>
+        </el-col>
+        <el-col :span="8">
+          <el-form-item :label="'域名3' + index" :prop="`domains.${index}.value3`" :rules="{required: true,message: '域名不能为空',trigger: 'blur',}">
+            <el-input v-model="domain.value3"></el-input>
+            <el-button @click.prevent="removeDomain(domain)">删除</el-button>
+          </el-form-item>
+        </el-col>
+
+      </el-row>
       <!-- 按钮 -->
       <el-form-item>
         <el-button type="primary" @click="submitForm('dynamicValidateForm')"
@@ -89,7 +110,9 @@ export default {
     //增加
     addDomain() {
       this.dynamicValidateForm.domains.push({
-        value: "",
+        value1: "",
+        value2: "",
+        value3: "",
         key: Date.now(),
       });
     },
