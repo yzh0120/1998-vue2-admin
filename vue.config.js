d4ee3f232f11fbc1d4d12a64f07c1ee628458832
yz <644193385@qq.com>
2024-12-18 17:27:54 +0800

    yp

--- vue.config.js
+++ vue.config.js
@@ -50,13 +50,13 @@ module.exports = {
         // }),
 
         // 使用gzip解压缩静态文件
-        new CompressionPlugin({
-          cache: false, // 不启用文件缓存
-          test: /\.(js|css|html)?$/i, // 压缩文件格式
-          filename: '[path].gz[query]', // 压缩后的文件名
-          algorithm: 'gzip', // 使用gzip压缩
-          minRatio: 0.8 // 压缩率小于1才会压缩
-        })
+        // new CompressionPlugin({
+        //   cache: false, // 不启用文件缓存
+        //   test: /\.(js|css|html)?$/i, // 压缩文件格式
+        //   filename: '[path].gz[query]', // 压缩后的文件名
+        //   algorithm: 'gzip', // 使用gzip压缩
+        //   minRatio: 0.8 // 压缩率小于1才会压缩
+        // })
       ],
     };
     const IS_PROD = ["production", "test", "uat"].includes(process.env.VUE_APP_ENV);
