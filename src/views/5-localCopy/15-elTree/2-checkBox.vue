1c2b8c3d80fb4e92ac18bcfd56df0f779fa71fcd
yz <644193385@qq.com>
2025-05-28 15:20:00 +0800

    up

--- src/views/5-localCopy/15-elTree/2-checkBox.vue
+++ src/views/5-localCopy/15-elTree/2-checkBox.vue
@@ -1,6 +1,7 @@
 <template>
   <page>
-    <el-button @click="set_checkbox">设置勾选</el-button>
+    <el-button @click="set_checkbox">设置勾选1</el-button>
+    <el-button @click="get_checkbox">获取勾选1</el-button>
     <el-tree
       show-checkbox
       @check-change="handleCheckChange"
@@ -19,18 +20,22 @@ export default {
       data: [
         {
           id: 1,
+          parentId:null,
           label: "一级 1",
           children: [
             {
-              id: 4,
+              id: 11,
+              parentId:1,
               label: "二级 1-1",
               children: [
                 {
-                  id: 9,
+                  id: 111,
+                  parentId:11,
                   label: "三级 1-1-1",
                 },
                 {
-                  id: 10,
+                  id: 112,
+                  parentId:11,
                   label: "三级 1-1-2",
                 },
               ],
@@ -42,33 +47,31 @@ export default {
           label: "一级 2",
           children: [
             {
-              id: 5,
+              id: 21,
               label: "二级 2-1",
             },
             {
-              id: 6,
+              id: 22,
               label: "二级 2-2",
             },
           ],
         },
-        {
-          id: 3,
-          label: "一级 3",
-          children: [
-            {
-              id: 7,
-              label: "二级 3-1",
-            },
-            {
-              id: 8,
-              label: "二级 3-2",
-            },
-          ],
-        },
+
       ],
     };
   },
   methods: {
+    get_checkbox(){
+      // console.log(this.$refs.tree.getCheckedNodes(false,false))
+      // console.log(this.$refs.tree.getCheckedKeys(false))
+      // console.log(this.$refs.tree.getHalfCheckedKeys())
+
+
+      // const CheckedNodes = this.$refs.tree.getCheckedNodes()
+      const CheckedKeys = this.$refs.tree.getCheckedKeys()
+      // const arr = [...CheckedKeys,this.$refs.tree.getHalfCheckedKeys()]
+      console.log([].concat(this.$refs.tree.getHalfCheckedKeys()).concat(CheckedKeys))
+    },
     // set_checkbox() {// node对象 的键值对 需要和 data里面的一样
     // 	this.$refs.tree.setCheckedNodes([{
     // 	             id: 10,
